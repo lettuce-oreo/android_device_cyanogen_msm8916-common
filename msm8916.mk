c107f54c42ca0a6a626cc6bd2097ded2a386964b
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-06-14 21:59:44 +0530

    msm8916-common: Nuke an msm8939 permissions xml
    Should've been a part of 915fb9a38a167e02591b10e23373cac0aeff362e, for some dumb reason I missed it

--- msm8916.mk
+++ msm8916.mk
@@ -137,11 +137,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
-endif
-
 # Power
 PRODUCT_PACKAGES += \
     power.msm8916
